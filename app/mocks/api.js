7dff19ba3b9a7413538c43fb1322d915f1f5a2dd
Cresten St. Clair <crestenstclair@Cresten-St-Clairss-MacBook-Pro.local>
2014-10-21 14:00:49 -0500

    Formatting fixes, getting rod of annoying grunt warnings

--- app/mocks/api.js
+++ app/mocks/api.js
@@ -1,45 +1,56 @@
 angular.module('video-ads.mockApi').run(
-   ['$httpBackend', 'mockVideoAdFactory',
+  ['$httpBackend', 'mockVideoAdFactory',
     function ($httpBackend, mockVideoAdFactory) {
       //TODO: Refine regex to be more precise, better reflect URL structure
       var videoAdListRegex = /^\/api\/v1\/videoads\/\?*/;
       var videoAdDetailRegex = /^\/api\/v1\/videoads\/[0-9]+\//;
       $httpBackend.whenGET(/partials/).passThrough();
       $httpBackend.whenGET(/exclusion\/global\/partials*/).passThrough();
-      $httpBackend.whenGET(/https\:\/\/app.zencoder\.com*/)
-      $httpBackend.whenGET(videoAdDetailRegex).respond(mockVideoAdFactory.videoad.detail);
-      $httpBackend.whenGET(videoAdListRegex).respond(mockVideoAdFactory.videoad.list);
+      $httpBackend.whenGET(/https\:\/\/app\.zencoder\.com*/);
+      $httpBackend.whenGET(videoAdDetailRegex).respond(
+        (function () {
+          console.log("Video Ad detail request received");
+          console.log(mockVideoAdFactory.videoad.detail);
+          return mockVideoAdFactory.videoad.detail;
+        }())
+      );
+      $httpBackend.whenGET(videoAdListRegex).respond(
+        (function () {
+          console.log("Video ad list request received");
+          console.log(mockVideoAdFactory.videoad.list);
+          return mockVideoAdFactory.videoad.list;
+        }())
+      );
       $httpBackend.whenPOST(videoAdListRegex).respond(200, 2);
+    }]
+).factory("mockVideoAdFactory", function () {
+  var _getVideoAd = function (id) {
+    var newVideoAd = _.clone(videoAd);
+    newVideoAd.id = _.isUndefined(id) ? _.random(100) : id;
+    newVideoAd.name += " " + newVideoAd.id;
+    //Create a random number for delivery for graph prettyness
+    newVideoAd.delivery = _.random(100);
+    return newVideoAd;
+  };
+
+  var _getListOfVideoAds = function (idsOfAds) {
+    var videoAdArray = [];
+    _.each(idsOfAds, function (adId) {
+      videoAdArray.push(_getVideoAd(adId));
+    });
+    return videoAdArray;
+  };
+
+  return {
+    "videoad": {
+      "list": {
+        "count": 20,
+        "next": "/api/v1/videoads/?page=2",
+        "previous": null,
+        "results": _getListOfVideoAds(_.range(5))
+      },
+      "detail": _getVideoAd()
     }
-  ]
-).factory("mockVideoAdFactory", function (){
-	var _getVideoAd = function(id){
-		var newVideoAd = _.clone(videoAd);
-        newVideoAd.id = _.isUndefined(id) ? _.random(100) : id;
-		newVideoAd.name += " " + newVideoAd.id;
-		//Create a random number for delivery for graph prettyness
-		newVideoAd.delivery = _.random(100);
-		return newVideoAd;
-	};
-
-	var _getListOfVideoAds = function(idsOfAds) {
-		var videoAdArray = [];
-		_.each(idsOfAds, function(adId){
-			videoAdArray.push(_getVideoAd(adId));
-		});
-		return videoAdArray;
-	};
-
-	return {
-		"videoad": {
-			"list": {
-					"count": 20,
-					"next": "/api/v1/videoads/?page=2",
-					"previous": null,
-					"results": _getListOfVideoAds(_.range(5))
-			},
-			"detail": _getVideoAd()
-		}
-	};
+  };
 
 });
