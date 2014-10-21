aa54a0c7bc53f0d28b867ecdd198c9cf308c7c04
Cresten St. Clair <crestenstclair@Cresten-St-Clairss-MacBook-Pro.local>
2014-10-21 14:21:51 -0500

    Formatting, to remove grunt errors

--- app/scripts/controllers/list.js
+++ app/scripts/controllers/list.js
@@ -1,45 +1,49 @@
 'use strict';
 
 angular.module('video-ads')
-	.controller('ListCtrl', function ($scope, $http, $location, videoAdService) {
-		$scope.videoads = [];
-		$scope.params = {};
-		$scope.showSearchBar = true;
+  .controller('ListCtrl', function ($scope, $location, videoAdService) {
+    $scope.videoads = [];
+    $scope.params = {};
+    $scope.showSearchBar = true;
 
-		$scope.updateList = function(){
-			$scope.params = _.extend($scope.params, $location.search());
-			if ($scope.params.filter === undefined) {
-				$scope.params.filter = "active";
-			}
-			videoAdService.getList([$scope.params]).then(function(data){
-				$scope.videoads = data;
-				$scope.totalItems = data.meta.count;
-			}, function(error){
-				console.log(error);
-			});
-		}
+    $scope.updateList = function () {
+      $scope.params = _.extend($scope.params, $location.search());
+      if ($scope.params.filter === undefined) {
+        $scope.params.filter = "active";
+      }
+      videoAdService.getList([$scope.params]).then(function (data) {
+        $scope.videoads = data;
+        $scope.totalItems = data.meta.count;
+      }, function (error) {
+        console.log(error);
+      });
+    };
+    
+    $scope.newVideoAd = function () {
+      videoAdService.post({
+        name: $scope.new_video_ad_name
+      })
+        .then(function (data) {
+          $location.path('/edit/' + data);
+        }, function (error) {
+          console.log(error);
+        });
+    };
 
-		$scope.newVideoAd = function(){
-			videoAdService.post({name: $scope.new_video_ad_name})
-			.then(function(data){
-				$location.path('/edit/' + data);
-			}, function(error){
-				console.log(error);
-			});
-		}
+    $scope.changePage = function (page) {
+      $location.search(_.extend($location.search(), {
+        'page': page
+      }));
+      $scope.updateList();
+    };
+    $scope.changeFilter = function (key, value) {
+      var obj = {};
+      obj[key] = value;
+      obj['page'] = 1;
+      $location.search(_.extend($location.search(), obj));
+      $scope.updateList();
+    };
 
-		$scope.changePage = function(page){
-			$location.search(_.extend($location.search(), {'page':page}));
-			$scope.updateList();
-		}
-		$scope.changeFilter = function(key, value){
-			var obj = {};
-			obj[key] = value;
-			obj['page'] = 1;
-			$location.search(_.extend($location.search(), obj));
-			$scope.updateList();
-		}
+    $scope.updateList();
 
-		$scope.updateList();
-
-	});
+  });
