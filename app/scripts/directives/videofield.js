7b3bda7519fb822f7e1c7592d63551334769fee1
Cresten St. Clair <cstclair@theonion.com>
2014-11-05 17:12:25 -0600

    Added the new files

---
+++ app/scripts/directives/videofield.js
@@ -0,0 +1,47 @@
+'use strict';
+
+angular.module('video-ads')
+  .directive('videoField', function (Zencoder, $scope) {
+    return {
+      templateUrl: 'views/partials/video-field.html',
+      restrict: 'E',
+      scope: {
+        advertisement: '=advertisement'
+      },
+      link: function postLink($scope) {
+        $scope.removeVideo = function () {
+          $scope.article.video = null;
+        };
+
+        $scope.uploadVideo = function () {
+          var additionalNewVideoParams = {'advertisement': $scope.advertisement.id};
+          Zencoder.onVideoFileUpload(additionalNewVideoParams).then(
+            function (success) {
+              console.log(success);
+              $scope.angularrticle.video = success.attrs.id;
+            },
+            function (error) {
+              console.log(error);
+            },
+            function (progress) {
+              console.log(progress);
+              $scope.uploadProgress = progress;
+            }
+          );
+        };
+
+        // $scope.thumbnailModal = function () {
+        //   Zencoder.openVideoThumbnailModal(article.video).result.then(
+        //     function (resolve) {
+        //       console.log('thumbnail modal resolve');
+        //       console.log(resolve);
+        //       //article.poster_url = resolve;
+        //     },
+        //     function (reject) {
+        //       console.log('thumbnail modal rejected');
+        //     }
+        //   );
+        // };
+      }
+    };
+  });
