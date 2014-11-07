c150bbe59922f369e2730ee91267d04367ee87c1
Cresten St. Clair <cstclair@theonion.com>
2014-11-07 13:52:20 -0600

    more progress

--- app/scripts/directives/videofield.js
+++ app/scripts/directives/videofield.js
@@ -13,7 +13,7 @@ angular.module('video-ads')
       link: function postLink($scope) {
 
         $scope.removeVideo = function() {
-          $scope.article.video = null;
+          $scope.video = null;
         };
 
         $scope.uploadVideo = function() {
@@ -21,13 +21,16 @@ angular.module('video-ads')
           var clickDeferred = $q.defer();
           fileField.bind('change', function(event) {
             var file = event.target.files[0];
-            $scope.validateVideoFile(file, clickDeferred);
-            $scope.addVideoToAdvertisement(file)
-              .then(function(response){
-                console.log(response);
-                clickDeferred.resolve();
-                // var s3Config = response.data.encoding_payload;
-                // Zencoder.uploadVideo($scope.file, s3Config);
+            //TODO: validation messaging.
+            $scope.validateVideoFile(file)
+              .then(function(){
+                  $scope.addVideoToAdvertisement(file)
+                .then(function(){
+                  var s3Config = $scope.video.encoding_payload;
+                  Zencoder.uploadVideo($scope.file, s3Config);
+                }).then(function(){
+                  clickDeferred.resolve();
+                });
               });
           });
           fileField.click();
@@ -35,24 +38,23 @@ angular.module('video-ads')
           return clickDeferred.promise;
         };
 
-        $scope.validateVideoFile = function(file, clickDeferred) {
+        $scope.validateVideoFile = function(file) {
+          var validateVideoFileDeferred = $q.defer();
           if (file) {
             if (file.size > (1024 * 1024 * 1024)) {
-              clickDeferred.reject('Upload file cannot be larger than 1024MB.');
+              validateVideoFileDeferred.reject('Upload file cannot be larger than 1024MB.');
             }
 
             if (file.type.indexOf('video/') !== 0) {
-              clickDeferred.reject('You must upload a video file.');
+              validateVideoFileDeferred.reject('You must upload a video file.');
             }
           } else {
-            clickDeferred.reject('Please select a file.');
+            validateVideoFileDeferred.reject('Please select a file.');
           }
-          return clickDeferred.resolve();
+          return validateVideoFileDeferred.resolve();
         };
 
         $scope.addVideoToAdvertisement = function(file) {
-          console.log(file);
-          console.log($scope.advertisementId);
           return $http.post('/api/videos/', {
             'advertisement': $scope.adid,
             'name': file.name
