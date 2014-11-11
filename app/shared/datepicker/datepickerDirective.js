fe4280a2fb014fcf7b8aad7df23daab07f45eb41
Cresten St. Clair <cstclair@theonion.com>
2014-11-11 15:09:59 -0600

    Datepicker directive finished

--- app/shared/datepicker/datepickerDirective.js
+++ app/shared/datepicker/datepickerDirective.js
@@ -1,9 +1,17 @@
 'use strict';
 angular.module('video-ads')
-  .directive('datepicker', function() {
+  .directive('datepicker', ['$filter', function($filter) {
     return {
+      scope: {
+        'date': '='
+      },
+      replace: true,
+      template: '<input ng-model="date" type="text"/>',
       link: function(scope, element){
-        element.datetimepicker();
+        element.datetimepicker({format: 'Y-m-d h:m'});
+        scope.$watch('date', function(){
+          scope.date = $filter('convertToLocal')(scope.date);
+        });
       }
     };
-  });
+  }]);
