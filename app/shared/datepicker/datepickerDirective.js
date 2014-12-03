1a6e20a530acf874d8d3f5157163266fda738db4
Chris Sinchok <csinchok@theonion.com>
2014-12-03 16:39:26 -0600

    A couple tweaks to start/end times

--- app/shared/datepicker/datepickerDirective.js
+++ app/shared/datepicker/datepickerDirective.js
@@ -3,18 +3,31 @@ angular.module('video-ads')
   .directive('datepicker', ['$filter', function($filter) {
     return {
       scope: {
-        'date': '='
+        'date': '=',
       },
       replace: true,
       require:'ngModel',
       template: '<input class="form-control" ng-model="date" type="text"/>',
       link: function(scope, element, attrs, ngModel){
         scope.required = attrs.required || false;
+
+        var startDate = new Date();
+        if (attrs.time === 'startOfDay') {
+          startDate.setMinutes(0);
+          startDate.setHours(0);
+          startDate.setSeconds(0);
+        }
+        if (attrs.time === 'endOfDay') {
+          startDate.setMinutes(59);
+          startDate.setHours(23);
+          startDate.setSeconds(59);
+        }
+
         element.datetimepicker({
           format: 'Y-m-d h:i A',
-          startDate: new Date(),
-          step: 15
-          });
+          startDate: startDate,
+          step: 30
+        });
         var formatter = function(value){
           return $filter('convertToLocal')(value);
         };
